4d5ab333565aac1b66e611f2ec89e28d25509e72
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-11-04 12:47:09 +0200

    Login and Register pages

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,26 @@
+import "../css/LR.scss";
+
+export default function Login() {
+  return (
+    <div className="login-App">
+      <div className="divp">
+        <h2 class="active"> Login </h2>
+        <h2 class="inactive underlineHover">
+          <a href="/register">Register</a>
+        </h2>
+      </div>
+      <div className="input-container">
+        <input type="text" placeholder="Email" />
+        <i class="zmdi zmdi-email zmdi-hc-lg"></i>
+      </div>
+
+      <div className="input-container">
+        <input type="password" placeholder="Password" />
+        <i class="zmdi zmdi-lock zmdi-hc-lg"></i>
+      </div>
+      <div class="container">
+        <button class="btn" data="Login"></button>
+      </div>
+    </div>
+  );
+}
