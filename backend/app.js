ec7217bbc5ace54b1dd7c204a6f034552e852a32
tomi <laine.tomi01@gmail.com>
2022-11-11 16:04:10 +0200

    Add CORS

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-unused-vars */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/userRoutes');
 
 var app = express();
 
@@ -13,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
