f0c985bfff76f8bc2dc93e92580dda5a32bec1e9
tomi <laine.tomi01@gmail.com>
2022-11-11 11:22:26 +0200

    Add JWT generation & verification

---
+++ backend/config/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { UNSAFE_NavigationContext } = require("react-router-dom");
+require("dotenv").config();
+
+function generateToken(user){
+    return jwt.sign({userId:user}, process.env.JWT_SECRET, {expiresIn: "1h"});
+}
+
+function verifyToken(token){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err){
+            console.log(err.message);
+            return res.status(403).send("Invalid token");
+        }
+        req.userId = user.userId;
+        next();
+    });
+}
+
+module.exports = {generateToken, verifyToken};
