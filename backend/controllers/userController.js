ab7ab7ce0216b9ef15ec12c49278b6928b7db8e5
Tommukka <laine.tomi01@gmail.com>
2022-11-12 18:39:33 +0200

    Fix login function to return JWT token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,11 +3,6 @@ const crypto = require("crypto");
 const jwt = require("../config/jwt");
 const user = require("../models/userModel");
 
-const username1 = "utest";
-//same as password1 = "ptest";
-const password1 =
-  "$2a$10$4njJjFDCPEkXmKPw3hnY5e2Z8tv5H/nUJVZhBjOmVFuKZjI0o2EV2";
-
 const login = (req, res) => {
   const { username, password } = req.body;
 
@@ -16,44 +11,33 @@ const login = (req, res) => {
       .status(400)
       .json({ message: "Please enter username and password." });
   }
-  // -- waiting db --
-  // user.getByUsername(username, (err, result) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return res.status(500).json({ message: "Internal server error." });
-  //   }
-  //   if (result.length === 0) {
-  //     return res.status(400).json({ message: "Invalid username or password." });
-  //   }
-  // bcrypt.compare(password, result[0].password, (err, match) => {
+  user.getByUsername(username, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Internal server error." });
+    }
+    if (result.length === 0) {
+      return res.status(400).json({ message: "Invalid username or password." });
+    }
 
-  if (username === username1) {
-    bcrypt.compare(password, password1, (err, match) => {
+    bcrypt.compare(password, result[0].password.toString(), (err, match) => {
       if (err) {
         return res.json(err);
       }
       if (match) {
-        // const token = jwt.generateToken(result[0].id);
+        const token = jwt.generateToken(result[0].user_ID);
         return res.status(200).json({
-          status: "success",
+          token: token,
           message: "Successfully logged in.",
         });
       } else {
         console.log("Invalid username or password!");
         return res.json({
-          status: "error",
           message: "Invalid username or password.",
         });
       }
     });
-  } else {
-    console.log("Invalid username or password!");
-    return res.json({
-      status: "error",
-      message: "Invalid username or password.",
-    });
-  }
-  // });
+  });
 };
 
 const register = (req, res) => {
@@ -67,9 +51,11 @@ const register = (req, res) => {
     return;
   } else {
     user.add(req, function (err, result) {
-      if (err.errno === 1062) {
-        res.status(400).json({ message: "Username already exists." });
-        return;
+      if (err) {
+        if (err.errno === 1062) {
+          res.status(400).json({ message: "Username already exists." });
+          return;
+        }
       }
       res.status(200).json({ message: "User registered successfully." });
     });
