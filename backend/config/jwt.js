c98d7eb63d616545cd10185a4c7b4c44391ea7a2
Tommukka <laine.tomi01@gmail.com>
2022-11-12 13:55:33 +0200

    Remove junk from jwt.js

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -1,24 +1,25 @@
 const jwt = require("jsonwebtoken");
-const { UNSAFE_NavigationContext } = require("react-router-dom");
 require("dotenv").config();
 
-function generateToken(user){
-    return jwt.sign({userId:user}, process.env.JWT_SECRET, {expiresIn: "1h"});
+function generateToken(user) {
+  return jwt.sign({ userId: user }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 }
 
-function verifyToken(req, res, next){
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if(token == null) return res.sendStatus(401);
+function verifyToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if(err){
-            console.log(err.message);
-            return res.status(403).send("Invalid token");
-        }
-        req.userId = user.userId;
-        next();
-    });
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log(err.message);
+      return res.status(403).send("Invalid token");
+    }
+    req.userId = user.userId;
+    next();
+  });
 }
 
-module.exports = {generateToken, verifyToken};
+module.exports = { generateToken, verifyToken };
