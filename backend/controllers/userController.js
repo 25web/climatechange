863f8f236b44a589733e23d2c326b72016c52515
Tommukka <laine.tomi01@gmail.com>
2022-11-12 13:56:05 +0200

    Fix logic check

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,28 +1,34 @@
-const bcrypt = require('bcrypt');
-const crypto = require('crypto');
-const jwt = require('../config/jwt');
-const user = require('../models/userModel');
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const jwt = require("../config/jwt");
+const user = require("../models/userModel");
 
 const register = (req, res) => {
-    if(!req.body.username || !req.body.password || !req.body.fname || !req.body.lname) {
-        res.status(400).json({message: "Please fill all fields."});
+  if (
+    !req.body.username ||
+    !req.body.password ||
+    !req.body.fname ||
+    !req.body.lname
+  ) {
+    res.status(400).json({ message: "Please fill all fields." });
+    return;
+  } else {
+    user.add(req, function (err, result) {
+      if (err.errno === 1062) {
+        res.status(400).json({ message: "Username already exists." });
         return;
-    }
-    user.add(req, function(err, result){
-        if(err.errno === 1062) {
-            res.status(400).json({message: "Username already exists."});
-            return;
-        }
-        res.status(200).json({message: "User registered successfully."});
+      }
+      res.status(200).json({ message: "User registered successfully." });
     });
-}
+  }
+};
 
 const getAllUsers = (req, res) => {
-    // FIXME
-    return res.status(200).json({message: "Return all users."});
-}
+  // FIXME
+  return res.status(200).json({ message: "Return all users." });
+};
 
 module.exports = {
-    register,
-    getAllUsers,
-}
+  register,
+  getAllUsers,
+};
