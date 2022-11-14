5cdbbbcab9aeaaf9b0b4338552f95bbac339d5b0
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-11-14 21:11:29 +0200

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Test from "./components/test";
+import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
+    <>
+      <div className="main">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../css/FNH.scss";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div class="container clearfix">
+        <div class="col" id="col-2">
+          <ul>
+            <li>ABOUT</li>
+            <li>
+              <p>Oamk website project</p>
+            </li>
+          </ul>
+        </div>
+        <div class="col" id="col-3">
+          <ul>
+            <li>ASDASD</li>
+            <li>
+              <a href="https://github.com/JoniPahikainen">Joni Pahikainen</a>
+            </li>
+            <li>
+              <a href="https://github.com/Kesonjus">Juha-Pekka Kesonen</a>
+            </li>
+            <li>
+              <a href="https://github.com/tomlnd">Tomi Laine</a>
+            </li>
+            <li>
+              <a href="https://github.com/Hilppah">Hilppa Huhtanen</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
