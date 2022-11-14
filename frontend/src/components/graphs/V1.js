791a96630c238984e3069901c1692d4f21f8a780
tomi <laine.tomi01@gmail.com>
2022-11-14 14:42:12 +0200

    Rename graphs -> charts

--- frontend/src/components/graphs/V1.js
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart } from 'chart.js/auto';
-import GetGraph from './GetGraph';
-
-export default function V1() {
-    console.log(GetGraph("v1"));
-}
