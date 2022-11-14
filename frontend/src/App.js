7917e00914980a93da762bb75ec3926994726326
tomi <laine.tomi01@gmail.com>
2022-11-14 14:39:55 +0200

    Add experimental route for getting chart data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
-import V1 from "./components/graphs/V1";
+import { GetChart } from "./components/charts/GetChart";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/graph" element={<V1 />} />
+        <Route path="/chart/:chartId" element={<GetChart />} />
       </Routes>
     </div>
   );
