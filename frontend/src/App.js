277caf50e2bbb42b5b6d2f7756ab2a8d8ebca27d
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-11-14 21:14:20 +0200

    footer cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Test from "./components/test";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/test" element={<Test />} />
         </Routes>
       </div>
       <div className="footer">
