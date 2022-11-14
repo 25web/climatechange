a8d3361f96e49681e295e2af50b9abb07d57dfcd
tomi <laine.tomi01@gmail.com>
2022-11-14 11:42:27 +0200

    Fix axios BaseURL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "http://localhost:3001";
 
 axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
