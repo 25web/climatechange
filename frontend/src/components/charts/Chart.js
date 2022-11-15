09d47a641800e49bc0518142b59c4847e0c73247
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-11-15 17:56:09 +0200

    working api chart.js

--- frontend/src/components/charts/Chart.js
+++ frontend/src/components/charts/Chart.js
@@ -2,45 +2,80 @@ import { GetChart } from "./GetChart";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import "../../css/chart.scss";
+import "chartjs-adapter-luxon";
 
 export function Chart() {
-  const YearsWeather = [
-    { year: '2019-02', temp: 10 },
-    { year: '2020-03', temp: 20 },
-    { year: '2021-03', temp: 30 },
-    { year: '2022-05', temp: 40 },
-    { year: '2023-05', temp: 50 },
-  ];
+  const [chartData, setChartData] = useState([]);
 
-//   let test = [];
-//   test = await GetChart("v1");
-//   console.log(test);
-console.log(GetChart("v1"));
-  const [chartData, setChartData] = useState({
-    labels: YearsWeather.map((d) => d.year),
+  useEffect(() => {
+    GetChart("/chart/v1", (res) => {
+      setChartData(res.data);
+    });
+  }, []);
+
+  let data = {
     datasets: [
       {
-        label: "global anomaly",
-        data: YearsWeather.map((d) => d.temp),
-        backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+        label: "global",
+        data: chartData,
+        borderColor: "rgb(4, 255, 46)",
+        backgroundColor: "rgb(4, 255, 46)",
+        parsing: {
+          xAxisKey: "year",
+          yAxisKey: "global_anomaly",
+        },
+        pointRadius: 0,
       },
-        // {
-        //   label: "northern anomaly",
-        //   data: test.map((d) => d.northern_anomaly),
-        //   backgroundColor: ["rgba(75, 192, 192, 0.6)"],
-        // },
-        // {
-        //   label: "southern anomaly",
-        //   data: test.map((d) => d.southern_anomaly),
-        // }
+      {
+        label: "southern",
+        data: chartData,
+        borderColor: "rgb(4, 226, 255)",
+        backgroundColor: "rgb(4, 226, 255)",
+        parsing: {
+          xAxisKey: "year",
+          yAxisKey: "southern_anomaly",
+        },
+        pointRadius: 0,
+      },
+      {
+        label: "northern",
+        data: chartData,
+        borderColor: "rgb(255, 4, 234)",
+        backgroundColor: "rgb(255, 4, 234)",
+        parsing: {
+          xAxisKey: "year",
+          yAxisKey: "northern_anomaly",
+        },
+        pointRadius: 0,
+      },
     ],
-  });
+  };
+  let optionsYear = {
+    responsive: true,
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "year",
+        },
+      },
+
+      y: {
+        min: -2,
+        max: 2,
+        ticks: {
+          stepSize: 0.3,
+        },
+      },
+    },
+  };
   return (
-    <div>
-      <Line data={chartData} />
-      <p>test</p>
-    </div>
+    <>
+      <div className="chart-wrapper">
+        <Line options={optionsYear} data={data} />
+      </div>
+    </>
   );
 }
 
