aa8b513983cf4f88a0ccda3d6662a98bb210d081
Juha-pekka Kesonen <kesonjus@gmail.com>
2022-11-22 01:10:03 +0200

    add V3 route

--- backend/routes/chartRoutes.js
+++ backend/routes/chartRoutes.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const jwt = require("../config/jwt");
 const chartController = require("../controllers/chartController");
 
-router.get("/v1", chartController.getV1monthly);
+router.get("/v1", chartController.getV1);
 
+router.get("/v3", chartController.getV3);
 module.exports = router;
