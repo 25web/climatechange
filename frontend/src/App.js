c5d695efeb9b8c7f83c8a5cd304368ba425de8f7
Juha-pekka Kesonen <kesonjus@gmail.com>
2022-11-22 01:08:52 +0200

    add V3 route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,15 @@ import Register from "./components/Register";
 import { GetChart } from "./components/charts/GetChart";
 import { Routes, Route } from "react-router-dom";
 import { V1Chart } from "./components/charts/V1";
-
+import { V3Chart } from "./components/charts/V3";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/chart" element={<V1Chart />} />
+        <Route path="/chart/v1" element={<V1Chart />} />
+        <Route path="/chart/v3" element={<V3Chart />} />
       </Routes>
     </div>
   );
