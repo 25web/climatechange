78fc986373f14db6ce31f0b0a4e8aba42147ebae
Juha-pekka Kesonen <kesonjus@gmail.com>
2022-11-22 01:10:44 +0200

    add V3 controller

--- backend/controllers/chartController.js
+++ backend/controllers/chartController.js
@@ -1,8 +1,8 @@
 const chartModel = require("../models/chartModel");
 const jwt = require("../config/jwt");
 
-const getV1monthly = (req, res) => {
-  chartModel.getV1monthly((err, result) => {
+const getV1 = (req, res) => {
+  chartModel.getV1((err, result) => {
     if (err) {
       console.log(err);
       return res.status(500).json({ message: "Internal server error." });
@@ -16,6 +16,22 @@ const getV1monthly = (req, res) => {
   });
 };
 
+const getV3 = (req, res) => {
+  chartModel.getV3((err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Internal server error." });
+    }
+    if (result.length === 0) {
+      return res.status(400).json({ message: "No data found." });
+    }
+    return res
+      .status(200)
+      .json({ resMonthly: result[0], resAnnual: result[1] });
+  });
+};
+
 module.exports = {
-  getV1monthly,
+  getV1,
+  getV3,
 };
