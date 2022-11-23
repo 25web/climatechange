fec0530eeb1cd9db46961048c7b6fa7707aa506c
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-11-23 19:02:39 +0200

    fix v2

--- backend/models/chartModel.js
+++ backend/models/chartModel.js
@@ -3,7 +3,7 @@ const db = require("../config/db");
 const chart = {
   getV1: function (callback) {
     return db.query(
-      "SELECT * FROM v1_monthly; SELECT * FROM v1_annual; SELECT * FROM v2",
+      "SELECT * FROM v1_monthly; SELECT * FROM v1_annual; SELECT * FROM v2;",
       callback
     );
   },
