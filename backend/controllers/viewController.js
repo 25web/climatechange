f85e4509f13cd8071f059a70816f81571d708b20
tomi <laine.tomi01@gmail.com>
2022-11-30 20:17:22 +0200

    Add custom views MVC

---
+++ backend/controllers/viewController.js
@@ -0,0 +1,39 @@
+const viewModel = require("../models/viewModel");
+const jwt = require("../config/jwt");
+const shortid = require("shortid");
+
+const getView = (req, res) => {
+  viewModel.getView(req.params.url, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Internal server error." });
+    }
+    if (result.length === 0) {
+      return res.status(400).json({ message: "No data found." });
+    }
+    return res.status(200).json(result);
+  });
+};
+
+const newView = (req, res) => {
+  if (!req.userId) {
+    return res.status(401).json({ message: "Unauthorized." });
+  }
+
+  const charts = req.body.charts;
+  const url = shortid.generate();
+
+  // Broken atm, need to fix
+  viewModel.newView(url, charts, req.userId, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Internal server error." });
+    }
+    return res.status(200).json({ message: "View created.", url: url });
+  });
+};
+
+module.exports = {
+  getView,
+  newView,
+};
