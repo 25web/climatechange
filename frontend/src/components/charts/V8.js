634d6972d2809c41c914a93585b95deb910022a1
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-12-04 23:33:11 +0200

    add requirements for v8

--- frontend/src/components/charts/V8.js
+++ frontend/src/components/charts/V8.js
@@ -9,6 +9,7 @@ export function V8Chart() {
   const [co2, setCo2] = useState([]);
   const [years, setYears] = useState([]);
   const [country, setCountry] = useState([]);
+  const [boolean, setBoolean] = useState(false);
 
   useEffect(() => {
     GetChart("/chart/v8", (res) => {
@@ -45,12 +46,73 @@ export function V8Chart() {
     }),
   };
 
-  const options = {};
+  const optionsYear = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: boolean ? false : true,
+        labels: {
+          font: {
+            size: 8.5,
+          },
+        },
+      },
+    },
+
+    responsive: false,
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: "Year",
+          font: {
+            size: 12,
+          },
+        },
+      },
+
+      y: {
+        stacked: true,
+        type: "linear",
+        title: {
+          display: true,
+          text: "Fossil CO2 emissions by country",
+          font: {
+            size: 10,
+          },
+        },
+      },
+    },
+  };
 
   return (
     <>
       <div className="chart-wrapper">
-        <Line options={options} data={data} />
+        <Line options={optionsYear} data={data} />
+        <button onClick={() => setBoolean(!boolean)}>Toggle labels</button>
+      </div>
+      <div>
+        <h4>Description</h4>
+        <p>CO2 emissions by country</p>
+      </div>
+      <div>
+        <h4>Source</h4>
+        <div className="inner">
+          <a
+            className="alink"
+            href="https://essd.copernicus.org/articles/14/1917/2022/"
+          >
+            Description
+          </a>
+        </div>
+        <div className="inner">
+          <a
+            className="alink"
+            href="https://data.icos-cp.eu/licence_accept?ids=%5B%22lApekzcmd4DRC34oGXQqOxbJ%22%5D"
+          >
+            Dataset
+          </a>
+        </div>
       </div>
     </>
   );
