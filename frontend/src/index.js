72fafd9cbb8ea4b67f29690f4438258df3d8846f
JoniPahikainen <joni.pahikainen01@gmail.com>
2022-12-13 02:27:47 +0200

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import "material-design-iconic-font/dist/css/material-design-iconic-font.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
