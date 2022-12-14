81d5b94fad394855f28791006f5a44a3ef32469f
tomi <laine.tomi01@gmail.com>
2022-12-14 19:18:23 +0200

    Add redirect to new custom view page

--- frontend/src/components/CustomView.js
+++ frontend/src/components/CustomView.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import "../css/FNH.scss";
 import axios from "../axios";
+import { useNavigate } from "react-router-dom";
 
 export default function CustomView() {
   const [charts, setCharts] = useState([]);
   const [title, setTitle] = useState("");
+  const navigate = useNavigate();
 
   const sortAscending = (arr) => {
     return arr.sort(function (a, b) {
@@ -21,69 +23,82 @@ export default function CustomView() {
     }
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
     const sortedCharts = sortAscending(charts);
+    if (/^[a-z0-9 ]+$/i.test(title) === false) {
+      alert("Title can only contain letters, numbers and spaces");
+      return;
+    }
+    if (sortedCharts.length === 0) {
+      alert("Please select at least one chart");
+      return;
+    }
     axios
-      .post("http://localhost:3001/view/create", {
-        title,
-        charts: sortedCharts,
-      })
+      .post(
+        "http://localhost:3001/view/create",
+        {
+          title,
+          charts: sortedCharts,
+        },
+        { headers: { "Cache-Control": "no-cache" } }
+      )
       .then((response) => {
-        console.log(response);
+        if (response.status === 200) {
+          navigate("/view/" + response.data.url);
+        }
       });
   };
 
   return (
-    <div className="checkbox">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          class="form-control"
-          id="title"
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Enter title"
-        />
-        <label>
-          <input type="checkbox" value="v1" onChange={handleCheckboxChange} />
-          V1
-        </label>
-        <label>
-          <input type="checkbox" value="v2" onChange={handleCheckboxChange} />
-          V2
-        </label>
-        <label>
-          <input type="checkbox" value="v3" onChange={handleCheckboxChange} />
-          V3
-        </label>
-        <label>
-          <input type="checkbox" value="v4" onChange={handleCheckboxChange} />
-          V4
-        </label>
-        <label>
-          <input type="checkbox" value="v5" onChange={handleCheckboxChange} />
-          V5
-        </label>
-        <label>
-          <input type="checkbox" value="v6" onChange={handleCheckboxChange} />
-          V6
-        </label>
-        <label>
-          <input type="checkbox" value="v7" onChange={handleCheckboxChange} />
-          V7
-        </label>
-        <label>
-          <input type="checkbox" value="v8" onChange={handleCheckboxChange} />
-          V8
-        </label>
-        <label>
-          <input type="checkbox" value="v9" onChange={handleCheckboxChange} />
-          V9
-        </label>
-        <br></br>
-        <button class="btn btn-success" type="submit">
-          Create new custom view
-        </button>
-      </form>
+    <div className="container">
+      <div className="checkbox">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter title"
+          />
+          <label>
+            <input type="checkbox" value="v1" onChange={handleCheckboxChange} />
+            V1
+          </label>
+          <label>
+            <input type="checkbox" value="v3" onChange={handleCheckboxChange} />
+            V3
+          </label>
+          <label>
+            <input type="checkbox" value="v4" onChange={handleCheckboxChange} />
+            V4
+          </label>
+          <label>
+            <input type="checkbox" value="v5" onChange={handleCheckboxChange} />
+            V5
+          </label>
+          <label>
+            <input type="checkbox" value="v6" onChange={handleCheckboxChange} />
+            V6
+          </label>
+          <label>
+            <input type="checkbox" value="v7" onChange={handleCheckboxChange} />
+            V7
+          </label>
+          <label>
+            <input type="checkbox" value="v8" onChange={handleCheckboxChange} />
+            V8
+          </label>
+          <label>
+            <input type="checkbox" value="v9" onChange={handleCheckboxChange} />
+            V9
+          </label>
+          <br></br>
+          <button class="btn btn-success" type="submit">
+            Create new custom view
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
